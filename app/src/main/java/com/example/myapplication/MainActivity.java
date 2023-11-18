be78472f9ed911492ff5a940bf653e1fec6995ae
HaQuocThach <thachhqbs00165@fpt.edu.vn>
2023-11-18 13:26:22 +0700

    as

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,28 +1,34 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-    
+    TextView logoutRedirectText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        View logoutButton = null;
-        logoutButton.setOnClickListener(new View.OnClickListener() {
+        logoutRedirectText = findViewById(R.id.logoutRedirectText);
+        logoutRedirectText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO: Add code to handle logout
-                // This could involve clearing the user's session data and then redirecting to the LoginActivity
-
                 Intent intent = new Intent(MainActivity.this, LogoutActivity.class);
                 startActivity(intent);
-                finish();  // This call is optional, it terminates the current activity
             }
         });
     }
